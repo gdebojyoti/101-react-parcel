491b6e9478eadb3e0c4f63f90da834507917b6a8
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-05 15:40:01 +0530

    Functional component.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ const headingElm = React.createElement('h1', { key: 'h1' }, 'I got in.')
 
 const subheadingElm = <h2 key='h2'>Let's see how it goes this time</h2>
 
-const containerElm = React.createElement('header', {}, [headingElm, subheadingElm])
+const ContentComponent = () => {
+  return <>My amazing content..</>
+}
+
+const containerElm = React.createElement('header', {}, [headingElm, subheadingElm, <ContentComponent key='content' />])
 
 const rootElm = document.getElementById('root')
 
