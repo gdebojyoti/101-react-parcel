ae13cf7a88c66677aee04e990434e50e00fc81eb
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-05 15:31:11 +0530

    JSX.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const headingElm = React.createElement('h1', { key: 'h1' }, 'I got in.')
-const subheadingElm = React.createElement('h2', { key: 'h2' }, 'Let\'s see how it goes this time')
+
+const subheadingElm = <h2 key='h2'>Let's see how it goes this time</h2>
+
 const containerElm = React.createElement('header', {}, [headingElm, subheadingElm])
 
 const rootElm = document.getElementById('root')
