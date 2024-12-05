dd3011d860edfa3323c1c6b8eed1857e16be2a40
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-05 15:10:23 +0530

    ReactDOM client fix. Nested children added.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
-const headingElm = React.createElement('h1', {}, 'I got in.')
+const headingElm = React.createElement('h1', { key: 'h1' }, 'I got in.')
+const subheadingElm = React.createElement('h2', { key: 'h2' }, 'Let\'s see how it goes this time')
+const containerElm = React.createElement('header', {}, [headingElm, subheadingElm])
+
 const rootElm = document.getElementById('root')
 
 const root = ReactDOM.createRoot(rootElm)
 
-root.render(headingElm)
+root.render(containerElm)
