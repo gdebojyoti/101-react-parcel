cb68b13d9b3f966c59e1e32c1fa2b4dce3b0d4fd
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2024-12-05 15:44:00 +0530

    Comments.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// react element
 const headingElm = React.createElement('h1', { key: 'h1' }, 'I got in.')
 
+// react element using jsx
 const subheadingElm = <h2 key='h2'>Let's see how it goes this time</h2>
 
+// functional component
 const ContentComponent = () => {
   return <>My amazing content..</>
 }
